1014e17e3567b784d95443caa0170cdb04e7dfbf
Muhammad Ramay <muhammad.ramay@catapulthealth.com>
2016-06-14 10:54:26 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const winston = require('winston');
+winston.add(winston.transports.File, { filename: 'server.log' });
+
+// Constants test
+const PORT = 8080;
+var requestNumber = 0;
+// App
+const app = express();
+app.get('/', function (request, response) {
+ requestNumber++;
+ winston.info('Got Request('+requestNumber+') on server ('+request.headers.host+')');
+  response.send('Service2 : Welcome to Jenkins + Docker Images in AWS POC by Muhammad Jaffar Ramay\nRequest('+requestNumber+')');
+});
+
+app.listen(PORT);
+winston.info('Running on http://localhost:' + PORT);
